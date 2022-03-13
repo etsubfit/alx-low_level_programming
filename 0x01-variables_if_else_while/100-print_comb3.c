b024116bf082b5a331b7608f9066efde4d4115d2
Etsub <eneyewmitiku@gmail.com>
2022-03-13 23:46:48 +0300

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    
-	for (int i = 0; i <= 9; i++)
-	{
-	    for (int j = i + 1; j <= 9; j++)
-	    {
-	        putchar(i + '0');
-	        putchar(j + '0');
-	        if (((i * 10) + j) < 89)
-	        {
-	            putchar(',');
-                putchar(' ');
+	int num1, num2;
 
-	        }
-	    }
+	for (num1 = '0'; num1 < '9'; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
+		{
+			if (num1 != num2)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 == '8' && num2 == '9')
+				{
+					continue;
+				}
+			}
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
-    return 0;
+	return (0);
 }
