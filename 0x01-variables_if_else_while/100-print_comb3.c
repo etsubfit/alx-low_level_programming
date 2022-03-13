fac40bebca0a754acac007b4e8ad2f569bf83dd7
Etsub <eneyewmitiku@gmail.com>
2022-03-13 23:40:25 +0300

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,38 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- **/
 int main(void)
 {
-	int d, p, q;
-
-	for (d = '0'; d < '9'; d++)
+    
+	for (int i = 0; i <= 9; i++)
 	{
-		for (p = d + 1; p <= '9'; p++)
-		{
-			for (q = p + 1; q <= '9'; q++)
-			{
-				if ((p != d) != q)
-				{
-					putchar(d);
-					putchar(p);
-					putchar(q);
-					if (d == '7' && p == '8')
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+	    for (int j = i + 1; j <= 9; j++)
+	    {
+	        putchar(i + '0');
+	        putchar(j + '0');
+	        if (((i * 10) + j) < 89)
+	        {
+	            putchar(',');
+                putchar(' ');
+
+	        }
+	    }
 	}
-
 	putchar('\n');
-
-	return (0);
-
+    return 0;
 }
